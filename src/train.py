a9964c560ce6f7bd7bdddf937a9ad67f084b9020
Sethu Gopalan <41025798+SethuGopalan@users.noreply.github.com>
2026-03-14 22:42:04 -0400

    Integrate MLflow for tracking experiments

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import yaml
+import mlflow
 
 def load_config():
     with open("configs/config.yaml", "r") as file:
@@ -6,7 +7,9 @@ def load_config():
     return config
 
 def main():
+    
     config = load_config()
+    mlflow.start_run()
     print("Project:", config["project_name"])
     print("Epochs:", config["training"]["epochs"])
     print("Batch size:", config["training"]["batch_size"])
